7b5ec9540eb80357487f645792b6c95b1c282eaa
Jai Sawhney <jai.sawhney1@gmail.com>
2021-10-12 14:00:08 -0700

    Dynamically create grid + redo cards

---
+++ js/cars.js
@@ -0,0 +1,37 @@
+const cars = [
+    {
+        name: "Nissan Leaf",
+        image: "../images/cars/nissan-leaf.png",
+        specs: {
+            range: "149 miles",
+            battery: "40 kWh",
+            power: "147 HP",
+            price: "$27,400"
+        },
+        learnMoreLink: "#"
+    },
+    {
+        name: "2021 Chevy Bolt",
+        image: "../images/cars/chevy-bolt.png",
+        specs: {
+            range: "259 miles",
+            battery: "66 kWh",
+            power: "200 HP",
+            price: "$36,500"
+        },
+        learnMoreLink: "#"
+    },
+    {
+        name: "Mini Cooper SE",
+        image: "../images/cars/mini-se.png",
+        specs: {
+            range: "110 miles",
+            battery: "32.64 kWh",
+            power: "181 HP",
+            price: "$29,900"
+        },
+        learnMoreLink: "#"
+    }
+]
+
+export default cars;
--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,61 @@
+const cards = document.querySelector("#cards");
+import data from "./cars.js"
+
+data.forEach(car => {
+    const card = document.createElement("div");
+    card.className = "card";
+
+    const cardImg = document.createElement("div");
+    cardImg.className = "card-img";
+
+    const img = document.createElement("img");
+    img.src = car.image;
+    img.alt = car.name;
+
+    const h3 = document.createElement("h3");
+    h3.innerHTML = car.name;
+
+    cardImg.append(img, h3);
+    card.append(cardImg);
+
+
+    const carSpecs = document.createElement("div");
+    carSpecs.className = "card-specs";
+
+
+    Object.keys(car.specs).forEach(spec => {
+        const container = document.createElement("div");
+        container.className = spec;
+
+        const h4 = document.createElement("h4");
+        h4.innerHTML = spec;
+        h4.className = "spec-name"
+
+        const p = document.createElement("p");
+        p.className = "gray-text";
+        p.innerHTML = car.specs[spec];
+
+        container.append(h4, p);
+        carSpecs.append(container);
+    });
+
+    const learnMore = document.createElement("a");
+    learnMore.className = "learn-more";
+    learnMore.innerHTML = "Learn More";
+    learnMore.href = data.learnMoreLink;
+
+    card.append(carSpecs, learnMore)
+    cards.append(card);
+});
+
+
 window.addEventListener("scroll", scroll);
 
 function scroll() {
     const homeSection = document.querySelector("#home");
     const navBar = document.querySelector("#navbar");
 
-    if (window.scrollY > (homeSection.offsetHeight - navBar.scrollHeight)) {
+    if (window.scrollY > (homeSection.offsetHeight - navBar.scrollHeight-1)) {
         navBar.classList.add("scrolled")
     } else {
         navBar.classList.remove("scrolled")
